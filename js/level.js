460bb37eb96319650bfa1d0032fcc136f2c38032
maxine bremner <maxine94bremner@gmail.com>
2014-06-04 18:44:01 +0100

    changed the width of the missile

--- js/level.js
+++ js/level.js
@@ -45,7 +45,7 @@
     'alien2': { sx: 0,  sy: 22, w: 38, h: 22, cls: Alien, frames: 3 }, 
     'alien3': { sx: 0,  sy: 33, w: 33, h: 20, cls: Alien, frames: 2 },  
     'player': { sx: 0,  sy: 84, w: 42, h: 53, cls: Player, frames: 3 },
-    'missile': { sx: 0, sy: 143, w: 5,  h: 14, cls: Missile }
+    'missile': { sx: 0, sy: 143, w: 3,  h: 14, cls: Missile }
    /* 'sheild': { sx: 0,  sy: 53, w: 45, h: 32, cls: Shield }, */
 
   }
